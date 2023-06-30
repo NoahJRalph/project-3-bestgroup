9bba4d4c72cfa078d273ed4ee751683e9b649651
Brian Morse <brian.p.morse@gmail.com>
2023-06-29 19:59:52 -0700

    created display, footer, and navbar components

---
+++ client/src/components/display.js
@@ -0,0 +1,8 @@
+//display area for other views
+function DisplayArea() {
+    return(
+        <main className='main-display'>stuff goes here </main>
+    )
+};
+
+export default DisplayArea;
---
+++ client/src/components/footer.js
@@ -0,0 +1,10 @@
+//footer
+function Footer() {
+    return(
+    <footer className='main-footer'>
+Site created and maintained by Noah Ralph, Aaron Richards, Monique Sherred, Philip Kubisz, and Brian Morse.
+    </footer>
+)
+}
+
+export default Footer;
