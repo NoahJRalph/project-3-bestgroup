8497391691f3fd94665cd437ea2421aafd929c0f
Monique <mo.sherred@gmail.com>
2023-07-01 16:03:38 -0700

    changed mongodb address to BrainSync

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api';
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/BrainSync';
 
 mongoose.connect(connectionString).catch(error => console.error('Error connecting to MongoDB', error))
 
