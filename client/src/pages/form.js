55220aaa8d650d17dd91d04226699e4bcb4765f3
Brian Morse <brian.p.morse@gmail.com>
2023-07-06 20:06:56 -0700

    added mutations for logging in, new posts, get all posts, and adding comments. Mutations need to be connected to react elements

--- client/src/pages/form.js
+++ client/src/pages/form.js
@@ -5,26 +5,27 @@ import { EmailIcon, LockIcon } from '@chakra-ui/icons';
 //? is this to sign in or to create a new user
 function Form() {
   return (
+    //I added id's to all of these elements thinking they were necessary to get specific information from different forms. If things are messing up, delete here first. Brian
     <Stack spacing={4}>
       <InputGroup>
         <InputLeftElement pointerEvents='none'>
           <EmailIcon color='gray.300' />
         </InputLeftElement>
-        <Input type='text' placeholder='Username' id='new-username' />
+        <Input type='text' placeholder='Username' id='newUsername' />
       </InputGroup>
 
       <InputGroup>
         <InputLeftElement pointerEvents='none'>
           <EmailIcon color='gray.300' />
         </InputLeftElement>
-        <Input type='email' placeholder='Email' id='new-email' />
+        <Input type='email' placeholder='Email' id='newEmail' />
       </InputGroup>
 
       <InputGroup>
         <InputLeftElement pointerEvents='none'>
           <LockIcon color='gray.300' />
         </InputLeftElement>
-        <Input type='password' placeholder='Password' id='new-password' />
+        <Input type='password' placeholder='Password' id='newPassword' />
       </InputGroup>
     </Stack>
   );
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,77 @@
 import { gql } from '@apollo/client';
 
+
+//add new user mutation
 export const ADD__NEW_USER = gql`
   mutation addUser($new-username: String!, $new-email: String!, $new-password: String!) {
-    addUser(username: $new-username, email: $new-email, password: $new-password) {
+    addUser(username: $newUsername, email: $newEmail, password: $newPassword) {
       token
       user {
         _id
         username
+      }
+    }
+  }
+`;
+
+
+//login mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// mutation for adding a new post
+export const ADD_POST = gql`
+  mutation addPost($postText: String!) {
+    addThought(thoughtText: $postText) {
+      _id
+      postText
+      postAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+//mutation for getting all posts
+export const GET_ALL_POSTS = gql`
+  query GetAllPosts {
+    allPosts {
+      _id
+      postText
+      postAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+//mutation for adding comments to posts
+export const ADD_COMMENT = gql`
+  mutation addComment($postId: ID!, $commentText: String!) {
+    addComment(thoughtId: $postId, commentText: $commentText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
       }
     }
   }
