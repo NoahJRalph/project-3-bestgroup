8cef5d8bbb0179145d8306e3b1c53a2da343d026
Monique <mo.sherred@gmail.com>
2023-07-11 20:43:53 -0700

    moved login from to login

--- client/src/components/loginOut/login.js
+++ client/src/components/loginOut/login.js
@@ -8,19 +8,54 @@ import {
   ModalCloseButton,
   Button,
   useDisclosure,
+  Input, Stack, InputGroup, InputLeftElement
 } from '@chakra-ui/react';
-import LoginForm from './loginForm';
+import { EmailIcon, LockIcon } from '@chakra-ui/icons'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useMutation } from '@apollo/client'
+import { LOGIN_USER } from '../../utils/mutations'
+import Auth from '../../utils/auth'
 import { useNavigate } from 'react-router-dom';
 
 function LoginModal() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const navigate = useNavigate();
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+  // update state based on form input changes
+  const handleChange = async (event) => {
+    const { name, value } = event.target
+    console.log('change happened')
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  }
 
-  // submit form and redirect to the dashboard
-  const handleFormSubmit = () => {
-    onClose();
-    navigate('/dashboard'); // Redirect to the dashboard
-  };
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault()
+    console.log('hi')
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      })
+      console.log({ data })
+      Auth.login(data.login.token)
+    } catch (e) {
+      console.error(e)
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    })
+    onClose()
+    navigate('/dashboard')
+  }
+
 
   return (
     <>
@@ -32,7 +67,52 @@ function LoginModal() {
           <ModalHeader>Login</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <LoginForm onSubmit={handleFormSubmit} />
+
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+
+              <form onSubmit={handleFormSubmit}>
+                <Stack spacing={4}>
+                  <InputGroup>
+                    <InputLeftElement pointerEvents='none'>
+                      <EmailIcon color='black' />
+                    </InputLeftElement>
+                    <Input
+                      type='email'
+                      placeholder='Email'
+                      name='email'
+                      value={formState.email}
+                      onChange={handleChange}
+                    />
+                  </InputGroup>
+
+                  <InputGroup>
+                    <InputLeftElement pointerEvents='none'>
+                      <LockIcon color='black' />
+                    </InputLeftElement>
+                    <Input
+                      type='password'
+                      placeholder='Password'
+                      name='password'
+                      value={formState.password}
+                      onChange={handleChange}
+                    />
+                  </InputGroup>
+                </Stack>
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+
+
           </ModalBody>
 
           <ModalFooter>
