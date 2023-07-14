744bdb4a5e8cfca9e75413d3ebad2de4da9ae935
Monique <mo.sherred@gmail.com>
2023-07-13 18:39:41 -0700

    update

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect('mongodb://127.0.0.1:27017/BrainSync'|| process.env.MONGO_URL , {
+		await mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/BrainSync', {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
